4d3bd60bff1a4ba85408a92026c2faa7e90857e3
jungjiyu <libraryofjiyu@gmail.com>
2024-09-09 18:21:20 +0900

    add : vLLM 활용부 추가 중

--- src/main/java/com/example/ai01/ai/vllm/service/VllmService.java
+++ src/main/java/com/example/ai01/ai/vllm/service/VllmService.java
@@ -1,22 +1,51 @@
 package com.example.ai01.ai.vllm.service;
 
-import com.example.ai01.ai.vllm.config.VllmConfig;
-import com.example.ai01.ai.vllm.dto.VllmRequest;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+
+@RequiredArgsConstructor
 @Service
 public class VllmService {
 
-    @Autowired
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
-    @Autowired
-    private VllmConfig vllmConfig;
+    @Value("${vllm.base.url}")
+    private String flaskApiUrl;
 
-    public String completeText(VllmRequest request ) {
-        String result = restTemplate.postForObject(vllmConfig.getBaseUrl() + "/complete", request, String.class);
-        return result;
+
+    public String analyzeImage(MultipartFile image) throws IOException {
+        String url = flaskApiUrl + "/analyze";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("image", new FileSystemResource(convert(image)));
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+
+        ResponseEntity<String> response = restTemplate.postForEntity(url, requestEntity, String.class);
+
+        return response.getBody();
     }
+
+    private File convert(MultipartFile file) throws IOException {
+        File convFile = new File(System.getProperty("java.io.tmpdir") + "/" + file.getOriginalFilename());
+        file.transferTo(convFile);
+        return convFile;
+    }
+
 }
